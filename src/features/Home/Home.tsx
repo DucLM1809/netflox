d4f651842680619ac13cc1c6919bc4a520cec939
minh duc <minhductxqt@gmail.com>
2023-03-06 22:53:32 +0700

    add header components

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useQuery } from 'react-query'
 import { getNetFlixOriginals } from '../../api/api'
 import Banner from '../../components/Banner/Banner'
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   const { isLoading, error, data, isFetching } = useQuery(
@@ -13,6 +14,7 @@ const Home = () => {
 
   return (
     <div className='relative h-screen bg-gradient-to-b lg:h-[140vh] overflow-hidden'>
+      <Header />
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={data} />
       </main>
