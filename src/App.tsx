75bfd5742e90804b1facd7f2dac4671e56f31f87
minh duc <minhductxqt@gmail.com>
2023-03-06 21:53:55 +0700

    add login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import { Suspense } from 'react'
-import { Layout } from 'antd'
 import { BrowserRouter, Routes } from 'react-router-dom'
 import { PrivateRoutes, PublicRoutes } from './routes/Router'
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense
-        fallback={
-          <Layout>
-            <>{'Loading...'}</>
-          </Layout>
-        }
-      >
-        <Layout>
-          <Routes>
-            {PublicRoutes}
-            {PrivateRoutes}
-          </Routes>
-        </Layout>
+      <Suspense fallback={<>{'Loading...'}</>}>
+        <Routes>
+          {PublicRoutes}
+          {PrivateRoutes}
+        </Routes>
       </Suspense>
     </BrowserRouter>
   )
