391a2cb9bfbe3bcd82144397da5f19ab7c7ff63a
minh duc <minhductxqt@gmail.com>
2023-03-06 23:20:38 +0700

    show all types of movies

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,34 @@
 import axios from 'axios'
 import requests from '../utils/requests'
 
-export const getNetFlixOriginals = axios
+export const getNetflixOriginals = axios
+  .get(requests.fetchNetflixOriginals)
+  .then((res) => res.data.results)
+
+export const getTrendings = axios
   .get(requests.fetchTrending)
+  .then((res) => res.data.results)
+
+export const getTopRated = axios
+  .get(requests.fetchTopRated)
+  .then((res) => res.data.results)
+
+export const getActionMovies = axios
+  .get(requests.fetchActionMovies)
+  .then((res) => res.data.results)
+
+export const getComedyMovies = axios
+  .get(requests.fetchComedyMovies)
+  .then((res) => res.data.results)
+
+export const getHorrorMovies = axios
+  .get(requests.fetchHorrorMovies)
+  .then((res) => res.data.results)
+
+export const getRomanceMovies = axios
+  .get(requests.fetchRomanceMovies)
+  .then((res) => res.data.results)
+
+export const getDocumentaries = axios
+  .get(requests.fetchDocumentaries)
   .then((res) => res.data.results)
--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,22 +1,108 @@
 import React from 'react'
 import { useQuery } from 'react-query'
-import { getNetFlixOriginals } from '../../api/api'
+import {
+  getActionMovies,
+  getComedyMovies,
+  getDocumentaries,
+  getHorrorMovies,
+  getNetflixOriginals,
+  getRomanceMovies,
+  getTopRated,
+  getTrendings
+} from '../../api/api'
 import Banner from '../../components/Banner/Banner'
 import Header from '../../components/Header/Header'
+import Row from '../../components/Row/Row'
 
 const Home = () => {
-  const { isLoading, error, data, isFetching } = useQuery(
-    'netflixOriginals',
-    () => getNetFlixOriginals
-  )
+  const {
+    isLoading: isLoadingNetflixOriginal,
+    error: errorLoadingNetflixOriginal,
+    data: netflixOriginals
+  } = useQuery('netflixOriginals', () => getNetflixOriginals)
 
-  if (isLoading) return <>Loading...</>
+  const {
+    isLoading: isLoadingTrending,
+    error: errorTrending,
+    data: trendingNow
+  } = useQuery('trendingNow', () => getTrendings)
+
+  const {
+    isLoading: isLoadingTopRated,
+    error: errorTopRated,
+    data: topRated
+  } = useQuery('topRated', () => getTopRated)
+
+  const {
+    isLoading: isLoadingActionMovies,
+    error: errorActionMovies,
+    data: actionMovies
+  } = useQuery('actionMovies', () => getActionMovies)
+
+  const {
+    isLoading: isLoadingComedyMovies,
+    error: errorComedyMovies,
+    data: comedyMovies
+  } = useQuery('comedyMovies', () => getComedyMovies)
+
+  const {
+    isLoading: isLoadingHorrorMovies,
+    error: errorHorrorMovies,
+    data: horrorMovies
+  } = useQuery('horrorMovies', () => getHorrorMovies)
+
+  const {
+    isLoading: isLoadingRomanceMovies,
+    error: errorRomanceMovies,
+    data: romanceMovies
+  } = useQuery('romanceMovies', () => getRomanceMovies)
+
+  const {
+    isLoading: isLoadingDocumentaries,
+    error: errorDocumentaries,
+    data: documentaries
+  } = useQuery('documentaries', () => getDocumentaries)
+
+  if (
+    isLoadingActionMovies ||
+    isLoadingComedyMovies ||
+    isLoadingHorrorMovies ||
+    isLoadingRomanceMovies ||
+    isLoadingDocumentaries ||
+    isLoadingTopRated ||
+    isLoadingTrending ||
+    isLoadingNetflixOriginal
+  )
+    return <>Loading...</>
+
+  if (
+    errorActionMovies ||
+    errorComedyMovies ||
+    errorHorrorMovies ||
+    errorRomanceMovies ||
+    errorDocumentaries ||
+    errorTopRated ||
+    errorTrending ||
+    errorLoadingNetflixOriginal
+  )
+    return <>Error...</>
 
   return (
-    <div className='relative h-screen bg-gradient-to-b lg:h-[140vh] overflow-hidden'>
+    <div className='relative h-screen bg-gradient-to-b lg:h-[140vh] '>
       <Header />
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
-        <Banner netflixOriginals={data} />
+        <Banner netflixOriginals={netflixOriginals} />
+        <section className='md:space-y-24'>
+          <Row title='Trending Now' movies={trendingNow} />
+          <Row title='Top Rated' movies={topRated} />
+          <Row title='Action Thrillers' movies={actionMovies} />
+          {/* My List */}
+
+          <Row title='Comedies' movies={comedyMovies} />
+          <Row title='Scary Movies' movies={horrorMovies} />
+          <Row title='Romance Movies' movies={romanceMovies} />
+          <Row title='Documentaries' movies={documentaries} />
+        </section>
       </main>
     </div>
   )
