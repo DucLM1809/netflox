4e38f61995e3097b02e5c3396f3ab6d51cb4d3f9
minh duc <minhductxqt@gmail.com>
2023-03-13 23:36:31 +0700

    remove unuse component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react'
 import { BrowserRouter, Routes } from 'react-router-dom'
 import Loading from './components/Loading/Loading'
-import Toast from './components/Toast/Toast'
 import { PrivateRoutes, PublicRoutes } from './routes/Router'
 
 function App() {
