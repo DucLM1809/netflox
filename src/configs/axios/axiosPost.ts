28235891af882f7af553561a647cd2c7c732b5a0
tungvtse <student2161999@gmail.com>
2023-03-16 00:26:56 +0700

    handle post tracking api

--- src/configs/axios/axiosPost.ts
+++ src/configs/axios/axiosPost.ts
@@ -2,6 +2,8 @@ import axiosClient from './axiosClient'
 
 const AxiosPost = async (url: string, data: any, params = {}) => {
   try {
+    console.log(url)
+
     const response = await axiosClient.post(url, data, { params })
     return Promise.resolve(response)
   } catch (error) {
